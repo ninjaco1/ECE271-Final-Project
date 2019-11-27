034e79900a32a14e6e2b962f7e1933bc47ddce4d
ninjaco1 <nguyean6@users.noreply.github.com>
2019-11-26 23:31:04 -0800

    i

--- FinalProject.sv
+++ FinalProject.sv
@@ -4,7 +4,11 @@ module FinalProject (input logic clk, ltch, data,
 							output logic led, motor,
 							output logic [6:0]seg);
 							
+	logic [7:0] a,b,s,y;
+	logic cin, cout;
 	
+							
+	add_sub #(8) (a,b,cin,s,y,cout);
 							
 	//log						
 							
--- add_sub.sv
+++ add_sub.sv
@@ -1,10 +1,14 @@
 // adder and substractor 
 
-module add_sub #(parameter = 8) 
+module add_sub #(parameter N = 8) 
 					(input logic [N-1:0] a,b,
 					input logic cin, 
 					output logic [N-1:0] s, y,
 					output logic cout); 
-					
-assign {cout, s} = a+b+cin; 
-assign y = a-b;
+		
+	adder #(N)(a,b, cin,s, cout);
+	substractor #(N)(a,b,y);
+	//assign {cout, s} = a+b+cin; 
+	//assign y = a-b;
+	
+endmodule
